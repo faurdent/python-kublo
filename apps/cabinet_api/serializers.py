35b94cb0e1e52c7a3850f79767f19404e737b7e2
Axeratos <nikitalukianitcares@gmail.com>
2023-08-16 23:23:59 +0300

    Serializer for user registration

---
+++ apps/cabinet_api/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+from apps.cabinet_api.models import CustomUser
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email", "password", "password2")
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError("Two passwords didn't match")
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop("password2")
+        return CustomUser.objects.create_user(**validated_data)
