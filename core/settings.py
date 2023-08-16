d3500119275dec17fd53fcb2a177ca8f396cbbeb
Axeratos <nikitalukianitcares@gmail.com>
2023-08-16 21:33:29 +0300

    Cabinet api app created

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "apps.cabinet_api.apps.CabinetApiConfig",
 ]
 
 MIDDLEWARE = [
