d7c996ffb1568d92e55d69f744ba6967ff6d6523
Axeratos <nikitalukianitcares@gmail.com>
2023-08-17 09:08:21 +0300

    Email unique validation added

--- apps/cabinet_api/serializers.py
+++ apps/cabinet_api/serializers.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from apps.cabinet_api.models import CustomUser
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=True)
+    email = serializers.EmailField(required=True, validators=[UniqueValidator(CustomUser.objects.all())])
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
